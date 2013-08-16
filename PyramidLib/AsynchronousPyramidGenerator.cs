5ea96467b5ebab5a0e48dd89bbd9a98da6a4c0f8
Tim McIver <tim@timmciver.com>
2013-08-15 20:47:56 -0400

    Added skeleton of code that recursively builds up all the Tasks that generate the tiles at all levels.

--- PyramidLib/AsynchronousPyramidGenerator.cs
+++ PyramidLib/AsynchronousPyramidGenerator.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Pyramid.Generator
 {
@@ -9,9 +11,43 @@ namespace Pyramid.Generator
 		{
 		}
 		
-		public void generate(StreamReader imageStream)
+		public async Task createTilesAsync(StreamReader imageStream)
 		{
+			// calculate the number of pyramid levels
+			int levels = 10;
 			
+			// wait for the top pyramid tile to be created
+			await createTileAsync(levels, 0, 0);
+			
+			// then write this final tile to file
+		}
+		
+		private async Task<Bitmap> createTileAsync(int level, int tileX, int tileY)
+		{
+			// check if we're at the base of the pyramid
+			if (level == 0)
+			{
+				// read a tile at the given coordinates and return the image
+			}
+			
+			// create tasks for the four child tiles in the level below this one
+			//Task<Bitmap>[] children = new Task<Bitmap>[4];
+			int[] xs = {2 * tileX, 2 * tileX + 1};
+			int[] ys = {2 * tileY, 2 * tileY + 1};
+			var topLeft = createTileAsync(level - 1, xs[0], ys[0]);
+			var topRight = createTileAsync(level - 1, xs[1], ys[0]);
+			var bottomLeft = createTileAsync(level - 1, xs[0], ys[1]);
+			var bottomRight = createTileAsync(level - 1, xs[1], ys[1]);
+			var children = new[] {topLeft, topRight, bottomLeft, bottomRight};
+			
+			// wait for all the children to finish
+			await Task.WhenAll(children);
+			
+			// stitch the four subtiles into one
+			
+			// then, write it to file
+			
+			// finally, return it
 		}
 	}
 }
