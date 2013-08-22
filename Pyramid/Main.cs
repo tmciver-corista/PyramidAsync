36378e6cecb7d55e88aad03dcb1a2adb6d5dcade
Tim McIver <tim.mciver@corista.com>
2013-08-22 19:04:45 -0400

    AsynchronousPyramidGenerator is now functional and has been verified to run in parallel on multicore machine.

--- Pyramid/Main.cs
+++ Pyramid/Main.cs
@@ -1,12 +1,31 @@
 using System;
+using System.Diagnostics;
+using Pyramid.Generator;
 
 namespace Pyramid
 {
 	class MainClass
 	{
-		public static void Main(string[] args)
+        const double READ_DURATION = 0.1;
+        const double STITCH_DURATION = 0.2;
+        const int TILE_DIMENSION = 256;
+        const int LEVELS = 3;
+		
+        public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// create a PyramidGenerator
+			PyramidGenerator generator = new AsynchronousPyramidGenerator(LEVELS, TILE_DIMENSION, READ_DURATION, STITCH_DURATION);
+
+            // start the timer
+            Stopwatch sw = Stopwatch.StartNew();
+			
+            // generate the pyramid
+            generator.generate(null);
+
+            // stop the timer
+            sw.Stop();
+
+            Console.WriteLine("Time taken: {0} ms", sw.Elapsed.TotalMilliseconds);
 		}
 	}
 }
--- PyramidLib/AsynchronousPyramidGenerator.cs
+++ PyramidLib/AsynchronousPyramidGenerator.cs
@@ -2,52 +2,94 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Diagnostics;
 
 namespace Pyramid.Generator
 {
 	public class AsynchronousPyramidGenerator : PyramidGenerator
 	{
-		public AsynchronousPyramidGenerator()
-		{
-		}
+        private int levels;
+        private int tileDimension;
+        private double readDuration;
+        private double stitchDuration;
 		
-		public async Task createTilesAsync(StreamReader imageStream)
+        public AsynchronousPyramidGenerator(int levels, int tileDimension, double readDuration, double stitchDuration)
 		{
-			// calculate the number of pyramid levels
-			int levels = 10;
-			
-			// wait for the top pyramid tile to be created
-			await createTileAsync(levels, 0, 0);
-			
-			// then write this final tile to file
+            this.levels = levels;
+            this.tileDimension = tileDimension;
+            this.readDuration = readDuration;
+            this.stitchDuration = stitchDuration;
 		}
+
+        public void generate(StreamReader imageStream)
+        {
+            createTilesAsync(imageStream);
+        }
 		
-		private async Task<Bitmap> createTileAsync(int level, int tileX, int tileY)
-		{
-			// check if we're at the base of the pyramid
-			if (level == 0)
-			{
-				// read a tile at the given coordinates and return the image
-			}
+        public void createTilesAsync(StreamReader imageStream)
+        {
+            // wait for the top pyramid tile to be created
+            createTileAsync(levels, 0, 0);
 			
-			// create tasks for the four child tiles in the level below this one
-			//Task<Bitmap>[] children = new Task<Bitmap>[4];
-			int[] xs = {2 * tileX, 2 * tileX + 1};
-			int[] ys = {2 * tileY, 2 * tileY + 1};
-			var topLeft = createTileAsync(level - 1, xs[0], ys[0]);
-			var topRight = createTileAsync(level - 1, xs[1], ys[0]);
-			var bottomLeft = createTileAsync(level - 1, xs[0], ys[1]);
-			var bottomRight = createTileAsync(level - 1, xs[1], ys[1]);
-			var children = new[] {topLeft, topRight, bottomLeft, bottomRight};
+            // then write this final tile to file
+        }
+		
+        private Bitmap createTileAsync(int level, int tileX, int tileY)
+        {
+            // check if we're at the base of the pyramid
+            if (level == 0)
+            {
+                // read a tile at the given coordinates and return the image
+                return readImage(tileX * tileDimension, tileY * tileDimension, tileDimension, tileDimension);
+            }
 			
-			// wait for all the children to finish
-			await Task.WhenAll(children);
+            // create tasks for the four child tiles in the level below this one
+            //Task<Bitmap>[] children = new Task<Bitmap>[4];
+            int[] xs = {2 * tileX, 2 * tileX + 1};
+            int[] ys = {2 * tileY, 2 * tileY + 1};
+            var topLeft = Task<Bitmap>.Factory.StartNew(() => { return createTileAsync(level - 1, xs[0], ys[0]); });
+            var topRight = Task<Bitmap>.Factory.StartNew(() => { return createTileAsync(level - 1, xs[1], ys[0]); });
+            var bottomLeft = Task<Bitmap>.Factory.StartNew(() => { return createTileAsync(level - 1, xs[0], ys[1]); });
+            var bottomRight = Task<Bitmap>.Factory.StartNew(() => { return createTileAsync(level - 1, xs[1], ys[1]); });
+            var children = new[] {topLeft, topRight, bottomLeft, bottomRight};
+
+            // wait for the tasks to finish
+            Task.WaitAll(topLeft, topRight, bottomLeft, bottomRight);
 			
-			// stitch the four subtiles into one
-			
-			// then, write it to file
-			
-			// finally, return it
-		}
+            // stitch the four subtiles into one and return it
+            return stitchImages(topLeft.Result, topRight.Result, bottomLeft.Result, bottomRight.Result);
+        }
+
+        private Bitmap stitchImages(Image topLeft, Image topRight, Image bottomLeft, Image bottomRight)
+        {
+
+            //Console.WriteLine("Stitching images . . .");
+
+            // wait stitchDuration seconds
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (sw.ElapsedMilliseconds < stitchDuration * 1000)
+            {
+                // do nothing
+            }
+
+            return new Bitmap(tileDimension, tileDimension); ;
+        }
+
+        private Bitmap readImage(int x, int y, int width, int height)
+        {
+            //Console.WriteLine("Reading image . . .");
+
+            // wait readDuration seconds
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            //Console.WriteLine("Millis: " + sw.ElapsedMilliseconds);
+            while (sw.ElapsedMilliseconds < readDuration * 1000)
+            {
+                // do nothing
+            }
+
+            return new Bitmap(tileDimension, tileDimension);
+        }
 	}
 }
